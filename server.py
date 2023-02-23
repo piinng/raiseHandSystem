2f19e5896ebeebf41467d3bb1185ce59bafa5ba0
sunyiping <52sunyiping@gmail.com>
2023-02-23 22:02:39 +0800

    test

---
+++ server.py
@@ -0,0 +1,66 @@
+import paho.mqtt.client as mqtt
+import sqlite3
+db=sqlite3.connect('lib.db')
+cursor=db.cursor()
+import json
+
+# 當地端程式連線伺服器得到回應時，要做的動作
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # 將訂閱主題寫在on_connet中
+    # 如果我們失去連線或重新連線時 
+    # 地端程式將會重新訂閱
+    client.subscribe("pingHandSystem/MQTT")
+
+# 當接收到從伺服器發送的訊息時要進行的動作
+def on_message(client, userdata, msg):
+    getmsg=json.dumps(str(msg.payload,encoding='utf-8'))
+    print(getmsg)
+    print(getmsg,type(getmsg))
+    if(getmsg.need=='getAndChangeIDStstus'):
+        if(getmsg.info=='hand'):
+            a="select handStatus from status where ID==\'%s\'"%(getmsg.ID)
+            cursor.execute(a)
+            if(cursor.fetchone()):
+                a="update status set handStatus=0 where ID == \'%s\'"%(getmsg.ID)
+                cursor.execute(a)
+            else:
+                a="update status set handStatus=1 where ID == \'%s\'"%(getmsg.ID)
+                cursor.execute(a)
+        elif(getmsg.info=='A'):
+            a="select AStatus from status where ID==\'%s\'"%(getmsg.ID)
+            cursor.execute(a)
+            if(cursor.fetchone()):
+                a="update status set AStatus=0 where ID == \'%s\'"%(getmsg.ID)
+                cursor.execute(a)
+            else:
+                a="update status set AStatus=1 where ID == \'%s\'"%(getmsg.ID)
+                cursor.execute(a)
+        elif(getmsg.info=='B'):
+            a="select BStatus from status where ID==\'%s\'"%(getmsg.ID)
+            cursor.execute(a)
+            if(cursor.fetchone()):
+                a="update status set BStatus=0 where ID == \'%s\'"%(getmsg.ID)
+                cursor.execute(a)
+            else:
+                a="update status set BStatus=1 where ID == \'%s\'"%(getmsg.ID)
+                cursor.execute(a)
+
+# 連線設定
+# 初始化地端程式
+client = mqtt.Client()
+
+# 設定連線的動作
+client.on_connect = on_connect
+
+# 設定接收訊息的動作
+client.on_message = on_message
+
+# 設定登入帳號密碼
+client.username_pw_set("","")
+
+# 設定連線資訊(IP, Port, 連線時間)
+client.connect("test.mosquitto.org", 1883, 60)
+
+client.loop_forever()
--- student.py
+++ student.py
@@ -1,22 +1,101 @@
 import tkinter as tk
+import paho.mqtt.client as mqtt
+import random
+import json  
+import datetime 
 import time
 
+IDnum='01'
+handStatus=0
+AStatus=0
+BStatus=0
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # 將訂閱主題寫在on_connet中
+    # 如果我們失去連線或重新連線時 
+    # 地端程式將會重新訂閱
+    client.subscribe("pingHandSystem/MQTT")
+
+'''def on_message(client, userdata, msg):
+    if(msg.payload.need=='sendStatus'):
+        if(msg.payload.info[0]):
+            hand_Button.config(background='yellow')
+        else:
+            hand_Button.config(background='gray')
+        if(msg.payload.info[1]):
+            A_Button.config(background='yellow')
+        else:
+            A_Button.config(background='gray')
+        if(msg.payload.info[2]):
+            B_Button.config(background='yellow')
+        else:
+            B_Button.config(background='gray')'''
+    
+client = mqtt.Client()
+
+# 設定連線的動作
+client.on_connect = on_connect
+
+# 設定接收訊息的動作
+#client.on_message = on_message
+
+# 設定登入帳號密碼
+client.username_pw_set("","")
+
+# 設定連線資訊(IP, Port, 連線時間)
+client.connect("test.mosquitto.org", 1883, 60)
+
+# 開始連線，執行設定的動作和處理重新連線問題
+# 也可以手動使用其他loop函式來進行連接
+client.loop_start()
+
 def fRename():
-    print(ID_entry.get())
+    t={
+        'From':'student',
+        'to':'lib',
+        'ID':IDnum,
+        'need':'rename',
+        'info':[name_entry.get()]
+    }
+    client.publish("pingHandSystem/MQTT",json.dumps(t))
 def fHang():
-    pass
+    t={
+        'From':'student',
+        'to':'lib',
+        'ID':IDnum,
+        'need':'getAndChangeIDStstus',
+        'info':['hand']
+    }
+    client.publish("pingHandSystem/MQTT",json.dumps(t))
 def fA():
-    pass
+    t={
+        'From':'student',
+        'to':'lib',
+        'ID':IDnum,
+        'need':'getAndChangeIDStstus',
+        'info':['A']
+    }
+    client.publish("pingHandSystem/MQTT",json.dumps(t))
 def fB():
-    pass
+    t={
+        'From':'student',
+        'to':'lib',
+        'ID':IDnum,
+        'need':'getAndChangeIDStstus',
+        'info':['B']
+    }
+    client.publish("pingHandSystem/MQTT",json.dumps(t))
 window = tk.Tk()
 window.title('學生端')
 
 #ID、rename
-ID_label=tk.Label(text="ID:")
-ID_entry=tk.Entry(width=10)
+ID_label=tk.Label(text="ID:%s"%(IDnum))
+name_entry=tk.Entry(width=10)
 ID_label.pack(side='left')
-ID_entry.pack(side='left')
+name_entry.pack(side='left')
 rename=tk.Button(text="👣",command=fRename,width=1)
 rename.pack(side='right')
 
